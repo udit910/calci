228a025e59f671acc28701a1e705c8f52a05bf8d
Udit Jain <ujain@walmartlabs.com>
2014-10-19 13:16:09 +0530

    added the main page and linked the activitites

--- app/src/main/java/udevs/calci/EMICalculatorActivity.java
+++ app/src/main/java/udevs/calci/EMICalculatorActivity.java
@@ -1,30 +1,21 @@
+
 package udevs.calci;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends Activity {
+public class EMICalculatorActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.emi_calculator_activity);
     }
 
-    public void startBasicCalculator(View view){
-        Intent intent= new Intent(this,CalculatorActivity.class);
-        startActivity(intent);
-    }
 
-    public void startEMICalculator(View view){
-        Intent intent= new Intent(this,EMICalculatorActivity.class);
-        startActivity(intent);
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
